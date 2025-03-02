871c8343227bca169cf8165247634f818bf347ed
Shameer Ebrahim <shameer66ster@gmail.com>
2025-03-02 22:11:50 +0530

    dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,10 +11,7 @@ export class TasksController {
     return this.taskService.getAllTasks();
   }
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.taskService.CreateTask(title, description);
+  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+    return this.taskService.CreateTask(CreateTaskDto);
   }
 }
